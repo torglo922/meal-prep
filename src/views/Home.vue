f044bd2e5385200ebe6798e3dab0ebb3e92b43db
Emil Naydyonov <enaydyonov96@inbox.ru>
2019-04-12 22:44:46 +0300

    add Home Hero, Details

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,19 @@
-<template></template>
+<template>
+  <span>
+    <home-hero></home-hero>
+    <home-details></home-details>
+  </span>
+</template>
 
 <script>
+import HomeHero from '@/components/HomeHero';
+import HomeDetails from '@/components/HomeDetails';
+
 export default {
   name: 'home',
-  components: {}
+  components: {
+    HomeHero,
+    HomeDetails
+  }
 };
 </script>
